0894be2ddc750dd935c9d6881763a3028da41673
Yasser Nasr <yasso1977@gmail.com>
2024-05-15 01:23:59 +0300

    commit from notebook

--- emcwebapp/settings.py
+++ emcwebapp/settings.py
@@ -154,19 +154,18 @@ MEDIA_URL = '/media/'
 #STATIC_URL = '/static/'
 #STATICFILEES_DIRS=os.path.join(BASE_DIR,'WbClinicVision/static')
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
-STATICFILEES_DIRS=os.path.join(BASE_DIR,'emcwebapp/static')
-django_heroku.settings(locals())
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATIC_URL = '/static/'
+# STATICFILEES_DIRS=os.path.join(BASE_DIR,'emcwebapp/static')
+# django_heroku.settings(locals())
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
