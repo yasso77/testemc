cdae1c17c29163befed4e5b63bd1be04aa201a8f
Yasser Nasr <yasso1977@gmail.com>
2024-05-26 09:48:30 +0300

    settings debug

--- emcwebapp/settings.py
+++ emcwebapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vlq84(*0n&$83qzdo-ytc^#5%u!*sb%@9_1a1d9z+_b#)l5%v3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
